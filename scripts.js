7202703957739eed5b9691929325bb4518f63120
Kyle Lamoureux <klamoureux386@gmail.com>
2020-02-11 11:44:40 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,13 +9,23 @@ container.setAttribute('class', 'container')
 app.appendChild(logo)
 app.appendChild(container)
 
+/*
 var request = new XMLHttpRequest()
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 request.onload = function() {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+  */
+    
+fetch('https://ghibliapi.herokuapp.com/films')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    // Work with JSON data here
+  
+  data.forEach(movie => {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
@@ -30,42 +40,23 @@ request.onload = function() {
       card.appendChild(h1)
       card.appendChild(p)
     })
+  
   } else {
     const errorMessage = document.createElement('marquee')
     errorMessage.textContent = `Gah, it's not working!`
     app.appendChild(errorMessage)
   }
+  
+    console.log(data)
+  })
+  .catch(err => {
+    // Do something for an error here
+  })
+
 }
 
 request.send()
 
-
-
-
-
-
-
-
-/*
-fetch('https://ghibliapi.herokuapp.com/films')
-  .then(response => {
-    return response.json()
-  })
- 
-  .then(data => {
-    // Work with JSON data here
-    
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response)
-
-  if (request.status >= 200 && request.status < 400) {
-  data.forEach(movie => {
-    console.log(movie.title)
-  })
-  } 
-  else {
-    console.log('error')
-  }
     
     console.log(data)
   })
